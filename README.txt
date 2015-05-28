36a5d50380ac22e931127c64844b96f69b6eb7c0
Mikael Mattsson <mikael@weblyan.se>
2015-05-28 16:56:31 +0200

    Update version number to 1.2.2

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: wallmanderco
 Tags: elasticsearch, indexer, performance, search engine
 Requires at least: 3.6
 Tested up to: 4.2
-Stable tag: 1.2.1
+Stable tag: 1.2.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- elasticsearch-indexer.php
+++ elasticsearch-indexer.php
@@ -14,7 +14,7 @@
  * Plugin URI: http://wallmanderco.github.io/elasticsearch-indexer/
  * Plugin Name: Elasticsearch Indexer
  * Description: Elasticsearch indexer for WordPress and WooCommerce
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: Mikael Mattsson
  * Text Domain: elasticsearch-indexer
  */
