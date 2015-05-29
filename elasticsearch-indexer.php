000ad401c1ebb1df22615e6cdc543803083692ac
Mikael Mattsson <mikael@weblyan.se>
2015-05-29 19:23:18 +0200

    Update version number to 1.2.3

--- elasticsearch-indexer.php
+++ elasticsearch-indexer.php
@@ -14,7 +14,7 @@
  * Plugin URI: http://wallmanderco.github.io/elasticsearch-indexer/
  * Plugin Name: Elasticsearch Indexer
  * Description: Elasticsearch indexer for WordPress and WooCommerce
- * Version: 1.2.2
+ * Version: 1.2.3
  * Author: Mikael Mattsson
  * Text Domain: elasticsearch-indexer
  */
